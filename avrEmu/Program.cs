dcd3ec3976d58e291b698ebd3ef51e2ba8e8f875
David Beinder <yoxxosh@gmail.com>
2012-06-06 08:14:31 +0200

    changed to unix line endings

--- avrEmu/Program.cs
+++ avrEmu/Program.cs
@@ -20,12 +20,20 @@ namespace avrEmu
 			
 			AvrPMFormLink fakeFlash = atny.ProgramMemory as AvrPMFormLink;
 			fakeFlash.FetchInstruction += delegate(object sender, FetchInstructionEventArgs e) {
-				e.Instruction = new AvrInstruction(instrs[e.InstructionNr]);
+                int iNr = e.InstructionNr % instrs.Length;
+				e.Instruction = new AvrInstruction(instrs[iNr]);
             };
+            DateTime start = DateTime.Now.Add(new TimeSpan(0,0,10));
+            int cnt = 0;
 
-            for (;;)
+            for (; ; )
+            {
+                cnt++;
                 atny.ClockTick();
-			
+                if(DateTime.Now > start)
+                    break;
+            }
+            Console.WriteLine((double)cnt/10.0);
             Console.ReadKey();
         }
     }
